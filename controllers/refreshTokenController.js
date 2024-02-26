01871f1ecee1d334ee25b1d88ff2ca888de16be3
Pouriya Chibaie <pouriya.chibaie.agh@gmail.com>
2024-02-26 11:39:19 +0330

    first commit

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,69 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const handleRefreshToken = async (req, res) => {
+  const refreshToken= req.body.refreshToken
+  if (!refreshToken) return res.sendStatus(401);
+
+
+  const foundUser = await User.findOne({ refreshToken }).exec();
+
+  if (!foundUser) {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) return;
+
+        const hackedUser = await User.findOne({
+          username: decoded.username,
+        }).exec();
+
+        hackedUser.refreshToken = [];
+        await hackedUser.save();
+      }
+    );
+    return res.sendStatus(403);
+  }
+
+  const newRefreshTokenArray = foundUser.refreshToken.filter(
+    (rt) => rt !== refreshToken
+  );
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        foundUser.refreshToken = [...newRefreshTokenArray];
+        await foundUser.save();
+      }
+
+      if (err || foundUser.username !== decoded.username)
+        return res.sendStatus(403);
+
+      const roles = Object.values(foundUser.roles);
+
+      const accessToken = jwt.sign(
+        {
+          userInfo: { username: decoded.username, roles },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "300s" }
+      );
+
+      const newRefreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+
+      foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+      await foundUser.save();
+
+      res.json({ roles, accessToken,newRefreshToken });
+    }
+  );
+};
+
+module.exports = { handleRefreshToken };
